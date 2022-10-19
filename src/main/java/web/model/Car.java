06cb1b06d3e21fa794f5d32cf3c81e458eaf2119
elena1989 <elena.shilova89@gmail.com>
2022-10-19 20:54:59 +0300

    создание car

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,31 @@
+package web.model;
+
+public class Car {
+    private String name;
+    private String series;
+    private int number;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
